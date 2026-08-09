35e3e8079d87992eda38f52ce2539f9dea7bb778
Naif R <naifrehman12@gmail.com>
2026-08-09 16:33:16 -0400

    Add: is-anagram - submission-19

---
+++ Data Structures & Algorithms/is-anagram/submission-19.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
+        
+        freqS = {}
+        for i in s:
+            freqS[i] = freqS.get(i, 0) +  1
+        
+        freqT = {}
+        for i in t:
+            freqT[i] = freqT.get(i, 0) + 1
+
+        if freqS == freqT:
+            return True
+        else:
+            return False
+
+        
+        
+        
