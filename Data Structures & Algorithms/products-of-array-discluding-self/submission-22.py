ee59e3dd8f41d45ec79cb71a5b953f5143e2f086
Naif R <naifrehman12@gmail.com>
2026-09-13 10:49:22 -0400

    Add: products-of-array-discluding-self - submission-22

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-22.py
@@ -0,0 +1,42 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        left = [1] * len(nums)
+        right = [1] *len(nums)
+
+        # populate the left array
+        leftMultiplier = 1
+        for i in range(len(nums)):
+            left[i] = leftMultiplier
+            leftMultiplier *= nums[i] # update number 
+        
+        # populate right array
+        rightMultiplier = 1
+        for i in range(len(nums) -1, -1, -1):
+            right[i] = rightMultiplier
+            rightMultiplier *= nums[i]
+
+        # create result arr
+        result = [1] * len(nums)
+        for i in range(len(result)):
+            result[i] = left[i] * right[i]
+        
+        return result
+        
+       
+
+        
+
+        
+        
+
+
+
+
+
+
+
+        
+
+
+        
