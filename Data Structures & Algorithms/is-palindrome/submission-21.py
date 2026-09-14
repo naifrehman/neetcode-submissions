6b5d428e69818a33d7af32f29fc800a3635bea6e
Naif R <naifrehman12@gmail.com>
2026-09-14 13:08:20 -0400

    Add: is-palindrome - submission-21

---
+++ Data Structures & Algorithms/is-palindrome/submission-21.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        # method 2 with 2 ptr's method
+        left = 0
+        right = len(s) -1
+
+        while left <= right:
+            # skip non-alphaNumeric char from left side 
+            while left < right and not s[left].isalnum():
+                left += 1
+            # skip non-alphanumeric from right side
+            while right > left and not s[right].isalnum():
+                right -= 1
+            
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+
+        return True
